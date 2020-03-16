427f900b3d7f05c6632b5bec51492c2766b717db
flyreal <meetflyreal@gmail.com>
2020-03-16 20:45:39 +0800

    complete basic config

---
+++ lib/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Button from "./button"
+
+ReactDOM.render(
+  <Button/>,
+  document.body
+)
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: {
+    clearui: "./lib/index.tsx"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "clearui",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", "jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ],
+  externals: {
+    "react": {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    }
+  }
+}
